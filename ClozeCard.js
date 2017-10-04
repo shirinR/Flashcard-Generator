99f3ca355d7a67b06ec92cf63db15ccb2e7ecb8d
shirinR <shirin.raad@gmail.com>
2017-10-03 21:38:45 -0700

    removed uselss line

--- ClozeCard.js
+++ ClozeCard.js
@@ -19,19 +19,19 @@ function ClozeCard(text, cloze){
   this.text = text;
   this.cloze = cloze; //contains only the cloze-deleted portion of the text.
   this.partial= text.split(cloze)[1]; //contains only the partial text.
-  this.fullText = text;   //contains only the full text.
+  // this.fullText = text;   //contains only the full text.
 }
 
 ClozeCard.prototype.checkMatch = function(){
   this.text.includes(this.cloze) ?
   console.log('we have the text') : function(err){
     if(err) throw err;
-    return;
+    // return;
   }
 };
 
 var clozeCard = new ClozeCard('George Washington was the first president of the United States.','George Washington');
 
-clozeCard.checkMatch("This doesn't work", "oops");
+clozeCard.checkMatch();
 
 module.export = ClozeCard;
